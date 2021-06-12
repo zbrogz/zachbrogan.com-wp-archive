e4576c83ac53870b14e806d9a5f69df7ff42aea1
zbrogz <zachbrogan@gmail.com>
2021-06-12 10:01:14 -0600

    Fix remaining bad urls

--- public/xmlrpc0db0.php
+++ public/xmlrpc0db0.php
@@ -2,13 +2,13 @@
 	<service>
 		<engineName>WordPress</engineName>
 		<engineLink>https://wordpress.org/</engineLink>
-		<homePageLink>https://zachbrogan.com</homePageLink>
+		<homePageLink>https://wp-archive.zachbrogan.com</homePageLink>
 		<apis>
-			<api name="WordPress" blogID="1" preferred="true" apiLink="https://zachbrogan.com/xmlrpc.php" />
-			<api name="Movable Type" blogID="1" preferred="false" apiLink="https://zachbrogan.com/xmlrpc.php" />
-			<api name="MetaWeblog" blogID="1" preferred="false" apiLink="https://zachbrogan.com/xmlrpc.php" />
-			<api name="Blogger" blogID="1" preferred="false" apiLink="https://zachbrogan.com/xmlrpc.php" />
-				<api name="WP-API" blogID="1" preferred="false" apiLink="https://zachbrogan.com/wp-json/" />
+			<api name="WordPress" blogID="1" preferred="true" apiLink="https://wp-archive.zachbrogan.com/xmlrpc.php" />
+			<api name="Movable Type" blogID="1" preferred="false" apiLink="https://wp-archive.zachbrogan.com/xmlrpc.php" />
+			<api name="MetaWeblog" blogID="1" preferred="false" apiLink="https://wp-archive.zachbrogan.com/xmlrpc.php" />
+			<api name="Blogger" blogID="1" preferred="false" apiLink="https://wp-archive.zachbrogan.com/xmlrpc.php" />
+				<api name="WP-API" blogID="1" preferred="false" apiLink="https://wp-archive.zachbrogan.com/wp-json/" />
 			</apis>
 	</service>
 </rsd>
